1bee984497db4a4ed90aac19e8c401caac7039d5
xuqiang521 <1257875587@qq.com>
2019-08-05 17:46:35 +0800

    improves multiple of promise

--- src/index.js
+++ src/index.js
@@ -8,50 +8,42 @@ const {
 } = recast.types.builders
 const t = recast.types.namedTypes
 
+function printSource (source) {
+  console.log(recast.print(source).code)
+}
+
 module.exports = function (source) {
   const ast = recast.parse(source, {
     parser: require('recast/parsers/babel')
-  })
-  let firstExp
-
-  recast.visit(ast, {
-    visitArrowFunctionExpression ({ node, parentPath }) {
-      const parentNode = parentPath.node
-      if (
-        t.CallExpression.check(parentNode) &&
-        t.Identifier.check(parentNode.callee.property) &&
-        parentNode.callee.property.name === 'then'
-      ) {
-        firstExp = node.body.body[0]
-      }
-      return false
-    }
   })
 
   recast.visit(ast, {
     visitCallExpression (path) {
       const { node } = path
-      let isArrowArg = false
+      const arguments = node.arguments
 
-      node.arguments.forEach(item => {
+      let firstExp
+
+      arguments.forEach(item => {
         if (t.ArrowFunctionExpression.check(item)) {
-          isArrowArg = true
+          firstExp = item.body.body[0]
+
+          if (
+            t.ExpressionStatement.check(firstExp) &&
+            t.Identifier.check(node.callee.property) &&
+            node.callee.property.name === 'then'
+          ) {
+            const arrowFunc = arrowFunctionExpression([], blockStatement([firstExp]))
+            const originFunc = callExpression(node.callee, node.arguments)
+            const catchFunc = callExpression(id('catch'), [arrowFunc])
+            const newFunc = memberExpression(originFunc, catchFunc)
+    
+            path.replace(newFunc)
+          }
         }
       })
 
-      if (
-        t.ExpressionStatement.check(firstExp) &&
-        t.Identifier.check(node.callee.property) &&
-        node.callee.property.name === 'then' &&
-        isArrowArg
-      ) {
-        const arrowFunc = arrowFunctionExpression([], blockStatement([firstExp]))
-        const originFunc = callExpression(node.callee, node.arguments)
-        const catchFunc = callExpression(id('catch'), [arrowFunc])
-        const newFunc = memberExpression(originFunc, catchFunc)
-
-        path.replace(newFunc)
-      }
+      
 
       return false
     }
--- test/demo.js
+++ test/demo.js
@@ -1,6 +1,13 @@
 this.p().then(res => {
   loading = false
   console.log('resolve')
+  // this.b().then(res => {
+  //   console.log('inner')
+  // })
+})
+
+this.p().then(res => {
+  console.log('resolve')
 })
 
 foo().then(() => {
